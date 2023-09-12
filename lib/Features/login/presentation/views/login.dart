259ce1691294cfe4e02e18da1935925482ed406e
Your Name <sjwdabwmrzwq65@gmail.com>
2023-09-12 11:56:43 +0300

    logun &singin

---
+++ lib/Features/login/presentation/views/login.dart
@@ -0,0 +1,105 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:noteapp/core/widgets/custombuttonauth.dart';
+import 'package:noteapp/core/widgets/customlogoauth.dart';
+import 'package:noteapp/core/widgets/textformfield.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: ListView(children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(height: 50),
+              const CustomLogoAuth(),
+              Container(height: 20),
+              const Text("Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              Container(height: 10),
+              const Text("Login To Continue Using The App",
+                  style: TextStyle(color: Colors.grey)),
+              Container(height: 20),
+              const Text(
+                "Email",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                  hinttext: "ُEnter Your Email", mycontroller: email),
+              Container(height: 10),
+              const Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                  hinttext: "ُEnter Your Password", mycontroller: email),
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 20),
+                alignment: Alignment.topRight,
+                child: const Text(
+                  "Forgot Password ?",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          CustomButtonAuth(title: "login", onPressed: () {}),
+          Container(height: 20),
+
+          MaterialButton(
+              height: 40,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              color: Colors.red[700],
+              textColor: Colors.white,
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Login With Google  "),
+                  Image.asset(
+                    "images/4.png",
+                    width: 20,
+                  )
+                ],
+              )),
+          Container(height: 20),
+          // Text("Don't Have An Account ? Resister" , textAlign: TextAlign.center,)
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed("signup") ;
+            },
+            child: const Center(
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "Don't Have An Account ? ",
+                ),
+                TextSpan(
+                    text: "Register",
+                    style: TextStyle(
+                        color: Colors.orange, fontWeight: FontWeight.bold)),
+              ])),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
