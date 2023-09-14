f7f73484055c9166e1edd16d8e5888f6e8a6936f
Your Name <sjwdabwmrzwq65@gmail.com>
2023-09-14 16:01:09 +0300

    تصحيح في login page

--- lib/Features/login/presentation/views/login.dart
+++ lib/Features/login/presentation/views/login.dart
@@ -27,7 +27,7 @@ class _LoginState extends State<Login> {
     }
     // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
+        await googleUser.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -183,7 +183,12 @@ setState(() {
                       )..show();
                     }
                   } on FirebaseAuthException catch (e) {
+                    isloading = false ;
+                    setState(() {
+
+                    });
                     if (e.code == 'user-not-found') {
+
                       AwesomeDialog(
                         context: context,
                         dialogType: DialogType.error,
