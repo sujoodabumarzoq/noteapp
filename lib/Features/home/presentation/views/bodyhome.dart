e341535935cff7747308e4ede3df0b4e740c5c42
Your Name <sjwdabwmrzwq65@gmail.com>
2023-09-14 15:32:51 +0300

    delete

--- lib/Features/home/presentation/views/bodyhome.dart
+++ lib/Features/home/presentation/views/bodyhome.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -11,19 +12,21 @@ class bodyhome extends StatefulWidget {
 }
 
 class _bodyhomeState extends State<bodyhome> {
-  List data =[];
+  List data = [];
+
   getdata() async {
-    QuerySnapshot querySnapshot =await   FirebaseFirestore.instance
-        .collection('categories')
-        .get() ;
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('categories').get();
     data.addAll(querySnapshot.docs);
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getdata();
   }
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
@@ -32,24 +35,46 @@ class _bodyhomeState extends State<bodyhome> {
             crossAxisCount: 2, mainAxisExtent: 160),
         itemCount: data.length,
         itemBuilder: (BuildContext context, i) {
-          return Card(
-              child: Container(
-            padding: EdgeInsets.all(10),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Image.asset("images/folder.png", height: 100, width: 100),
-                   Text(
-                    "${data[i] ['addcetogry']}",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
-                  ),
-                ],
+          return InkWell(
+            onTap: () {
+              AwesomeDialog(
+                context: context,
+                dialogType: DialogType.warning,
+                animType: AnimType.rightSlide,
+                title: 'ERROR',
+                desc: 'Pare sure of the deleting process................',
+                btnCancelOnPress: () {},
+                btnOkOnPress: () async {
+                  await FirebaseFirestore.instance
+                      .collection('categories')
+                      .doc(data[i].id)
+
+                      .delete();
+                  Navigator.of(context).pushReplacementNamed("Home");
+
+                  // print("$id");
+                },
+              )..show();
+            },
+            child: Card(
+                child: Container(
+              padding: EdgeInsets.all(10),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Image.asset("images/folder.png", height: 100, width: 100),
+                    Text(
+                      "${data[i]['addcetogry']}",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ));
+            )),
+          );
         });
   }
 }
